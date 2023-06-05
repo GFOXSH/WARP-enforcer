dcbc7780c335384370c7ee5b94bb305097782b0f
GFOXSH <gfoxsh@bk.ru>
2023-06-05 11:22:54 +0300

    Update 2.0

--- Unit1.dfm
+++ Unit1.dfm
@@ -1,9 +1,10 @@
 object Form1: TForm1
-  Left = 0
-  Top = 0
-  Width = 251
-  Height = 115
-  Caption = 'Basic WARP'
+  Left = 338
+  Top = 354
+  BorderStyle = bsDialog
+  Caption = 'WARP Enforcer'
+  ClientHeight = 286
+  ClientWidth = 202
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -18,45 +19,156 @@ object Form1: TForm1
   object Edit1: TEdit
     Left = 0
     Top = 0
-    Width = 161
+    Width = 201
     Height = 21
     TabOrder = 0
   end
-  object Button1: TButton
-    Left = 72
-    Top = 48
-    Width = 75
-    Height = 25
-    Caption = 'Launch'
-    TabOrder = 1
-    OnClick = Button1Click
-  end
-  object ComboBox1: TComboBox
-    Left = 168
-    Top = 0
-    Width = 65
-    Height = 21
-    Style = csDropDownList
-    ItemHeight = 13
-    ItemIndex = 0
-    TabOrder = 2
-    Text = 'None'
-    Items.Strings = (
-      'None'
-      '9_1'
-      '9_2'
-      '9_3'
-      '10_0'
-      '10_1'
-      '11_0'
-      '11_1')
-  end
-  object CheckBox1: TCheckBox
+  object Edit2: TEdit
     Left = 0
     Top = 24
-    Width = 233
-    Height = 17
-    Caption = 'Disable feature level upgrade'
+    Width = 201
+    Height = 21
+    TabOrder = 1
+  end
+  object GroupBox1: TGroupBox
+    Left = 8
+    Top = 48
+    Width = 185
+    Height = 97
+    Caption = 'WARP'
+    TabOrder = 2
+    object CheckBox1: TCheckBox
+      Left = 8
+      Top = 16
+      Width = 97
+      Height = 17
+      Caption = 'Enable'
+      TabOrder = 0
+    end
+    object ComboBox1: TComboBox
+      Left = 8
+      Top = 40
+      Width = 169
+      Height = 21
+      ItemHeight = 13
+      ItemIndex = 0
+      TabOrder = 1
+      Text = 'None'
+      Items.Strings = (
+        'None'
+        '9_1'
+        '9_2'
+        '9_3'
+        '10_0'
+        '10_1'
+        '11_0'
+        '11_1')
+    end
+    object CheckBox2: TCheckBox
+      Left = 8
+      Top = 72
+      Width = 169
+      Height = 17
+      Caption = 'Disable Feature Level Upgrade'
+      TabOrder = 2
+    end
+  end
+  object GroupBox2: TGroupBox
+    Left = 8
+    Top = 152
+    Width = 185
+    Height = 97
+    Caption = 'CPU'
     TabOrder = 3
+    object CheckBox3: TCheckBox
+      Left = 8
+      Top = 16
+      Width = 97
+      Height = 17
+      Caption = 'Enable'
+      TabOrder = 0
+    end
+    object ComboBox2: TComboBox
+      Left = 8
+      Top = 40
+      Width = 169
+      Height = 21
+      ItemHeight = 13
+      ItemIndex = 3
+      TabOrder = 1
+      Text = 'Normal'
+      Items.Strings = (
+        'Realtime'
+        'High'
+        'Above normal'
+        'Normal'
+        'Below normal'
+        'Low')
+    end
+    object CheckBox4: TCheckBox
+      Left = 24
+      Top = 72
+      Width = 17
+      Height = 17
+      TabOrder = 2
+    end
+    object CheckBox5: TCheckBox
+      Left = 40
+      Top = 72
+      Width = 17
+      Height = 17
+      TabOrder = 3
+    end
+    object CheckBox6: TCheckBox
+      Left = 56
+      Top = 72
+      Width = 17
+      Height = 17
+      TabOrder = 4
+    end
+    object CheckBox7: TCheckBox
+      Left = 72
+      Top = 72
+      Width = 17
+      Height = 17
+      TabOrder = 5
+    end
+    object CheckBox8: TCheckBox
+      Left = 88
+      Top = 72
+      Width = 17
+      Height = 17
+      TabOrder = 6
+    end
+    object CheckBox9: TCheckBox
+      Left = 104
+      Top = 72
+      Width = 17
+      Height = 17
+      TabOrder = 7
+    end
+    object CheckBox10: TCheckBox
+      Left = 120
+      Top = 72
+      Width = 17
+      Height = 17
+      TabOrder = 8
+    end
+    object CheckBox11: TCheckBox
+      Left = 136
+      Top = 72
+      Width = 17
+      Height = 17
+      TabOrder = 9
+    end
+  end
+  object Button1: TButton
+    Left = 8
+    Top = 256
+    Width = 185
+    Height = 25
+    Caption = 'Launch'
+    TabOrder = 4
+    OnClick = Button1Click
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,142 +4,250 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, Registry, ShellAPI;
+  Dialogs, StdCtrls, Registry, ShellAPI, IniFiles;
 
 type
   TForm1 = class(TForm)
     Edit1: TEdit;
-    Button1: TButton;
-    ComboBox1: TComboBox;
+    Edit2: TEdit;
+    GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
     CheckBox1: TCheckBox;
-    procedure Button1Click(Sender: TObject);
+    ComboBox1: TComboBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
+    ComboBox2: TComboBox;
+    CheckBox4: TCheckBox;
+    CheckBox5: TCheckBox;
+    CheckBox6: TCheckBox;
+    CheckBox7: TCheckBox;
+    CheckBox8: TCheckBox;
+    CheckBox9: TCheckBox;
+    CheckBox10: TCheckBox;
+    CheckBox11: TCheckBox;
+    Button1: TButton;
     procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
   private
-    { Private declarations }
+    procedure ConfigRead;
+    procedure OnProcessStart;
+    procedure ConfigWrite;
   public
     { Public declarations }
   end;
-  ingame = class(TThread)
+  WaitForProcess = class(TThread)
   private
-    procedure Button;
+    procedure OnProcessEnd;
+    procedure ButtonUpdate;
+    procedure ProcessConfig;
   protected
     procedure Execute; override;
   end;
 
 var
   Form1: TForm1;
-  reg:TRegistry;
-  exeinf: TShellExecuteInfo;
-  isactive:Boolean;
+  ExeInf: TShellExecuteInfo;
+  IsActive: Boolean;
+  PFileName, PDirectoryPath: PChar;
+  ProcessPriority, ProcessAffinity: Dword;
 
 implementation
 
 {$R *.dfm}
 
-procedure ingame.Execute;
-begin
-if ShellExecuteEx(@exeinf) then
-begin
-isactive:=true;
-Synchronize(Button);
-WaitforSingleObject(exeinf.hProcess, INFINITE);
-CloseHandle(exeinf.hProcess);
-end;
-reg:=TRegistry.Create;
-try
-reg.RootKey:=HKEY_CURRENT_USER;
-reg.OpenKey('Software\Microsoft\Direct3D\Application0', true);
-reg.WriteString('D3DBehaviors', 'FeatureLevelLimit=0;ForceWARP=0;DisableFLUpgrade=0');
-reg.WriteString('Name', '');
-finally
-reg.Free;
-end;
-isactive:=false;
-Synchronize(Button);
-end;
-
-procedure ingame.Button;
-begin
-if isactive then
-Form1.Button1.Caption:='Break'
-else
-Form1.Button1.Caption:='Launch';
-end;
-
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TForm1.ConfigRead;
 var
-fll,flu:Integer;
-name:String;
+  IniFile: TIniFile;
 begin
-if isactive then
-TerminateProcess(exeinf.hProcess, 0)
-else
+  IniFile:=TIniFile.Create(GetCurrentDir+'\WARP.ini');
+  Edit1.Text:=IniFile.ReadString('Main', 'FileName', 'DELTARUNE.exe');
+  Edit2.Text:=IniFile.ReadString('Main', 'DirectoryPath', GetCurrentDir+ '\');
+  CheckBox1.Checked:=IniFile.ReadBool('WARP', 'Enable', false);
+  ComboBox1.ItemIndex:=IniFile.ReadInteger('WARP', 'FeatureLevelLimit', 0);
+  CheckBox2.Checked:=IniFile.ReadBool('WARP', 'DisableFeatureLevelUpgrade', false);
+  CheckBox3.Checked:=IniFile.ReadBool('CPU', 'Enable', false);
+  ComboBox2.ItemIndex:=IniFile.ReadInteger('CPU', 'Priority', 3);
+  CheckBox4.Checked:=IniFile.ReadBool('CPU', 'Core0', true);
+  CheckBox5.Checked:=IniFile.ReadBool('CPU', 'Core1', true);
+  CheckBox6.Checked:=IniFile.ReadBool('CPU', 'Core2', true);
+  CheckBox7.Checked:=IniFile.ReadBool('CPU', 'Core3', true);
+  CheckBox8.Checked:=IniFile.ReadBool('CPU', 'Core4', true);
+  CheckBox9.Checked:=IniFile.ReadBool('CPU', 'Core5', true);
+  CheckBox10.Checked:=IniFile.ReadBool('CPU', 'Core6', true);
+  CheckBox11.Checked:=IniFile.ReadBool('CPU', 'Core7', true);
+  IniFile.Free;
+end;
+
+procedure TForm1.OnProcessStart;
+var
+  FileName, DirectoryPath: AnsiString;
+  FeatureLevelLimit, DisableFeatureLevelUpgrade: Integer;
+  Registry: TRegistry;
+  CpuSet: set of 0..31;
 begin
-name:=Edit1.Text;
-case ComboBox1.ItemIndex of
-0:fll:=0;
-1:fll:=StrToInt('$9100');
-2:fll:=StrToInt('$9200');
-3:fll:=StrToInt('$9300');
-4:fll:=StrToInt('$a000');
-5:fll:=StrToInt('$a100');
-6:fll:=StrToInt('$b000');
-7:fll:=StrToInt('$b100');
-else fll:=0;
+  FileName:=Edit1.Text;
+  GetMem(PFileName, Length(FileName) + 1);
+  StrPLCopy(PFileName, FileName, Length(FileName) + 1);
+  DirectoryPath:=Edit2.Text;
+  GetMem(PDirectoryPath, Length(DirectoryPath) + 1);
+  StrPLCopy(PDirectoryPath, DirectoryPath, Length(DirectoryPath) + 1);
+  if CheckBox1.Checked then
+  begin
+    case ComboBox1.ItemIndex of
+      0:FeatureLevelLimit:=0;
+      1:FeatureLevelLimit:=StrToInt('$9100');
+      2:FeatureLevelLimit:=StrToInt('$9200');
+      3:FeatureLevelLimit:=StrToInt('$9300');
+      4:FeatureLevelLimit:=StrToInt('$a000');
+      5:FeatureLevelLimit:=StrToInt('$a100');
+      6:FeatureLevelLimit:=StrToInt('$b000');
+      7:FeatureLevelLimit:=StrToInt('$b100');
+    else
+      FeatureLevelLimit:=0;
+    end;
+    if CheckBox2.Checked then
+      DisableFeatureLevelUpgrade:=1
+    else
+      DisableFeatureLevelUpgrade:=0;
+    Registry:=TRegistry.Create;
+    try
+      Registry.RootKey:=HKEY_CURRENT_USER;
+      Registry.OpenKey('Software\Microsoft\Direct3D\Application0', true);
+      Registry.WriteString('Name', FileName);
+      Registry.WriteString('D3DBehaviors', 'ForceWARP=1;FeatureLevelLimit='+IntToStr(FeatureLevelLimit)+';DisableFLUpgrade='+IntToStr(DisableFeatureLevelUpgrade));
+    finally
+      Registry.Free;
+    end;
+  end;
+  if CheckBox3.Checked then
+  begin
+    case Combobox2.ItemIndex of
+      0:ProcessPriority:=$00000100;
+      1:ProcessPriority:=$00000080;
+      2:ProcessPriority:=$00008000;
+      3:ProcessPriority:=$00000020;
+      4:ProcessPriority:=$00004000;
+      5:ProcessPriority:=$00000040;
+    else
+      ProcessPriority:=$00000020;
+    end;
+    CpuSet:=[];
+    if CheckBox4.Checked then
+      Include(CpuSet, 0);
+    if CheckBox5.Checked then
+      Include(CpuSet, 1);
+    if CheckBox6.Checked then
+      Include(CpuSet, 2);
+    if CheckBox7.Checked then
+      Include(CpuSet, 3);
+    if CheckBox8.Checked then
+      Include(CpuSet, 4);
+    if CheckBox9.Checked then
+      Include(CpuSet, 5);
+    if CheckBox10.Checked then
+      Include(CpuSet, 6);
+    if CheckBox11.Checked then
+      Include(CpuSet, 7);
+    ProcessAffinity:=Dword(CpuSet);
+  end;
+  FillChar(ExeInf,SizeOf(ExeInf), 0);
+  with ExeInf do
+  begin
+    cbSize:=SizeOf(ExeInf);
+    fMask:=SEE_MASK_NOCLOSEPROCESS;
+    lpFile:=PFileName;
+    lpDirectory:=PDirectoryPath;
+    nShow:=SW_SHOWNORMAL;
+  end;
+  WaitForProcess.Create(false);
 end;
-if CheckBox1.Checked then
-flu:=1
-else
-flu:=0;
-reg:=TRegistry.Create;
-try
-reg.RootKey:=HKEY_CURRENT_USER;
-reg.OpenKey('Software\Microsoft\Direct3D\Application0', true);
-reg.WriteString('D3DBehaviors', 'FeatureLevelLimit='+IntToStr(fll)+';ForceWARP=1;DisableFLUpgrade='+IntToStr(flu));
-reg.WriteString('Name', name);
-finally
-reg.Free;
-end;
-FillChar(exeinf,SizeOf(exeinf), 0);
-with exeinf do
+  
+
+procedure WaitForProcess.Execute;
 begin
-cbSize:=SizeOf(exeinf);
-fMask:=SEE_MASK_NOCLOSEPROCESS;
-lpFile:=PChar(name);
-nShow:=SW_SHOWNORMAL;
+  if ShellExecuteEx(@ExeInf) then
+  begin
+    IsActive:=true;
+    Synchronize(ButtonUpdate);
+    Synchronize(ProcessConfig);
+    WaitforSingleObject(ExeInf.hProcess, INFINITE);
+    CloseHandle(ExeInf.hProcess);
+    IsActive:=false;
+    Synchronize(ButtonUpdate);
+    Synchronize(OnProcessEnd);
+  end;
 end;
-ingame.Create(false);
+
+procedure WaitForProcess.ButtonUpdate;
+begin
+  if IsActive then
+    Form1.Button1.Caption:='Break'
+  else
+    Form1.Button1.Caption:='Launch';
 end;
+
+procedure WaitForProcess.ProcessConfig;
+begin
+  if Form1.CheckBox3.Checked then
+  begin
+    SetPriorityClass(exeinf.hProcess, ProcessPriority);
+    SetProcessAffinityMask(exeinf.hProcess, ProcessAffinity);
+  end;
+end;
+
+procedure WaitForProcess.OnProcessEnd;
+var
+  Registry: TRegistry;
+begin
+  Registry:=TRegistry.Create;
+  try
+    Registry.RootKey:=HKEY_CURRENT_USER;
+    Registry.DeleteKey('Software\Microsoft\Direct3D\Application0');
+  finally
+    Registry.Free;
+  end;
+  FreeMem(PFileName);
+  FreeMem(PDirectoryPath);
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-filename:String;
 begin
-isactive:=false;
-filename:=ExtractFileName(Application.ExeName);
-case filename[1] of
-'0':ComboBox1.ItemIndex:=0;
-'1':ComboBox1.ItemIndex:=1;
-'2':ComboBox1.ItemIndex:=2;
-'3':ComboBox1.ItemIndex:=3;
-'4':ComboBox1.ItemIndex:=4;
-'5':ComboBox1.ItemIndex:=5;
-'6':ComboBox1.ItemIndex:=6;
-'7':ComboBox1.ItemIndex:=7;
-'8':ComboBox1.ItemIndex:=0;
-'9':ComboBox1.ItemIndex:=1;
-'A':ComboBox1.ItemIndex:=2;
-'B':ComboBox1.ItemIndex:=3;
-'C':ComboBox1.ItemIndex:=4;
-'D':ComboBox1.ItemIndex:=5;
-'E':ComboBox1.ItemIndex:=6;
-'F':ComboBox1.ItemIndex:=7;
+  IsActive:=false;
+  ConfigRead;
 end;
-if filename[1] in ['8', '9', 'A', 'B', 'C', 'D', 'E', 'F'] then
-CheckBox1.Checked:=true;
-Edit1.Text:=Copy(filename, 2, Length(filename));
 
+procedure TForm1.ConfigWrite;
+var
+  IniFile: TIniFile;
+begin
+  IniFile:=TiniFile.Create(GetCurrentDir+'\WARP.ini');
+  IniFile.WriteString('Main', 'FileName', Edit1.Text);
+  IniFile.WriteString('Main', 'DirectoryPath', Edit2.Text);
+  IniFile.WriteBool('WARP', 'Enable', CheckBox1.Checked);
+  IniFile.WriteInteger('WARP', 'FeatureLevelLimit', ComboBox1.ItemIndex);
+  IniFile.WriteBool('WARP', 'DisableFeatureLevelUpgrade', CheckBox2.Checked);
+  IniFile.WriteBool('CPU', 'Enable', CheckBox3.Checked);
+  IniFile.WriteInteger('CPU', 'Priority', ComboBox2.ItemIndex);
+  IniFile.WriteBool('CPU', 'Core0', CheckBox4.Checked);
+  IniFile.WriteBool('CPU', 'Core1', CheckBox5.Checked);
+  IniFile.WriteBool('CPU', 'Core2', CheckBox6.Checked);
+  IniFile.WriteBool('CPU', 'Core3', CheckBox7.Checked);
+  IniFile.WriteBool('CPU', 'Core4', CheckBox8.Checked);
+  IniFile.WriteBool('CPU', 'Core5', CheckBox9.Checked);
+  IniFile.WriteBool('CPU', 'Core6', CheckBox10.Checked);
+  IniFile.WriteBool('CPU', 'Core7', CheckBox11.Checked);
+  IniFile.Free;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if IsActive then
+    TerminateProcess(ExeInf.hProcess, 0)
+  else
+  begin
+    OnProcessStart;
+    ConfigWrite;
+  end;
 end;
 
 end.
